7e9622d132cbdc90988da0bd71c2d4d5fdce705d
Rob Pickerill <r.pickerill@gmail.com>
2024-01-01 09:38:33 +0000

    add initial ci

--- src/deletion/deleter.rs
+++ src/deletion/deleter.rs
@@ -15,3 +15,17 @@ impl Deleter {
         input.chars().filter(|c| !self.chars.contains(c)).collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_delete() {
+        let deleter = Deleter::new("abc");
+
+        assert_eq!(deleter.delete("abc"), "");
+        assert_eq!(deleter.delete("cba"), "");
+        assert_eq!(deleter.delete("abccba"), "");
+    }
+}
